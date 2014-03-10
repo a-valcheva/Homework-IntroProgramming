beec8e8ac51d928145410e3686306b126a4fb4df
anichka777 <avalcheva7@gmail.com>
2014-03-10 03:03:10 +0200

    Changes

--- 15.AgeAfter10Years/AgeAfter10Years.cs
+++ 15.AgeAfter10Years/AgeAfter10Years.cs
@@ -2,10 +2,20 @@
 
 class AgeAfter10Years
 {
+    //Old
     //* Write a program to read your age from the console and print how old you will be after 10 years.
+
+    //New
+    //Write a program to read your birthday from the console and print how old you are now and how old you will be after 
+    //10 years.
     static void Main(string[] args)
     {
-        
+        //Old
+        //Console.WriteLine("Enter your age");
+        //int ageOld = int.Parse(Console.ReadLine());
+        //Console.WriteLine("After 10 years you will be {0} years old", ageOld + 10);
+
+        //New
         Console.WriteLine("Enter your birthday(try yyyy.m.d)");
         DateTime birthDay = DateTime.Parse(Console.ReadLine());
         DateTime today = DateTime.Now;
