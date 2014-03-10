2e2431bc3e2925ec3a42171a5bcde2e96b31151c
anichka777 <avalcheva7@gmail.com>
2014-03-10 02:56:09 +0200

    Changes

--- 15.AgeAfter10Years/AgeAfter10Years.cs
+++ 15.AgeAfter10Years/AgeAfter10Years.cs
@@ -10,12 +10,7 @@ class AgeAfter10Years
         DateTime birthDay = DateTime.Parse(Console.ReadLine());
         DateTime today = DateTime.Now;
         int age = today.Year - birthDay.Year;
-        if (birthDay.Month <= today.Month && birthDay.Day <= today.Day)
-        {
-            Console.WriteLine("You are {0} years old.", age);
-            Console.WriteLine("After 10 years you will be {0} years old", age + 10);
-        }
-        else if (birthDay.Month < today.Month && birthDay.Day > today.Day)
+        if ((birthDay.Month <= today.Month && birthDay.Day <= today.Day) ^ (birthDay.Month < today.Month && birthDay.Day > today.Day))
         {
             Console.WriteLine("You are {0} years old.", age);
             Console.WriteLine("After 10 years you will be {0} years old", age + 10);
